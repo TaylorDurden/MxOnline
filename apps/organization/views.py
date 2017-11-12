5ca13ec4e702ce84c83a63373fc01df560e011ce
Taylor <258266864@qq.com>
2017-11-12 23:06:26 +0800

    完成分页，开始做筛选功能

--- apps/organization/views.py
+++ apps/organization/views.py
@@ -2,6 +2,8 @@
 from django.shortcuts import render
 from django.views.generic import View
 
+from pure_pagination import Paginator, EmptyPage, PageNotAnInteger
+
 from .models import CourseOrg, CityDict
 # Create your views here.
 
@@ -14,8 +16,18 @@ class OrgView(View):
         all_orgs = CourseOrg.objects.all()
         org_count = all_orgs.count()
         all_cities = CityDict.objects.all()
+
+        #对课程机构进行分页
+        try:
+            page = request.GET.get('page', 1)
+        except PageNotAnInteger:
+            page = 1
+
+        p = Paginator(all_orgs, 2, request=request)
+        paged_orgs = p.page(page)
+
         return render(request, "org-list.html", {
-            "all_orgs": all_orgs,
+            "all_orgs": paged_orgs,
             "all_cities": all_cities,
             "org_count": org_count
         })
